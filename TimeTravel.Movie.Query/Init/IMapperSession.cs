51c16091c28343a7eea27d2aa42d36f94b2f779a
chaidrinker <info@indianmuslim.org.uk>
2020-06-30 11:39:38 +0100

    Add cession class and interface

---
+++ TimeTravel.Movie.Query/Init/IMapperSession.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeTravel.Movie.Query.Init
+{
+    public interface IMapperSession
+    {
+        void BeginTransation();
+        Task Commit();
+        Task Rollback();
+        void CloseTransaction();
+        Task Save(Core.Models.Movie entity);
+        Task Delete(Core.Models.Movie entity);
+
+        IQueryable<Core.Models.Movie> Movies { get; }
+    }
+}
---
+++ TimeTravel.Movie.Query/Init/MapperSession.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NHibernate;
+
+namespace TimeTravel.Movie.Query.Init
+{
+    public class MapperSession : IMapperSession
+    {
+        private readonly ISession _session;
+        private ITransaction _transaction;
+
+        public MapperSession(ISession session)
+        {
+            _session = session;
+        }
+
+        public void BeginTransation()
+        {
+            _transaction = _session.BeginTransaction();
+        }
+
+        public async Task Commit()
+        {
+            await _transaction.CommitAsync();
+        }
+
+        public async Task Rollback()
+        {
+            await _transaction.RollbackAsync();
+        }
+
+        public void CloseTransaction()
+        {
+            if (_transaction != null)
+            {
+                _transaction.Dispose();
+                _transaction = null;
+            }
+        }
+
+        public async Task Save(Core.Models.Movie entity)
+        {
+            await _session.SaveOrUpdateAsync(entity);
+        }
+
+        public async Task Delete(Core.Models.Movie entity)
+        {
+            await _session.DeleteAsync(entity);
+        }
+
+        public IQueryable<Core.Models.Movie> Movies => _session.Query<Core.Models.Movie>();
+    }
+}
